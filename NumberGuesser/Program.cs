1b87edc188ff7c05e05985f1cf2cb452c1d30776
ZnoGouDj <znogoud3@gmail.com>
2022-03-25 23:52:52 +0100

    first commit

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry Point Method
+        static void Main(string[] args)
+        {
+            string name = "Eric Andre";
+            int age = 35;
+
+            Console.WriteLine(name + " is " + age);
+            Console.WriteLine("{0} is {1}", name, age);
+        }
+    }
+}
